3492c7dffb6df428e482f1477af8c8b9258d16fc
corka149 <corka149@mailbox.org>
2017-09-28 19:53:40 +0200

    implemented query() and added sigmoid

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ extern crate rand;
 pub mod util;
 pub mod vector;
 
+use vector::math;
+
 struct NeuralNetwork<T>
-    where T: Fn(i64) -> i64
+    where T: Fn(f64) -> f64
 {
     input_nodes: u64,
     hidden_nodes: u64,
@@ -18,7 +20,7 @@ struct NeuralNetwork<T>
 }
 
 impl<T> NeuralNetwork<T>
-    where T: Fn(i64) -> i64
+    where T: Fn(f64) -> f64
 {
     pub fn new(input_nodes: u64,
                hidden_nodes: u64,
@@ -35,41 +37,65 @@ impl<T> NeuralNetwork<T>
             output_nodes,
             learning_rate,
             activation_function,
-            wih,
-            who,
+            wih, // input -> hidden
+            who, // hidden -> output
         }
     }
 
-    pub fn train(&self, inputs: &[i8], output: &[i8]) {}
+    pub fn train(&self, inputs: Vec<f64>, output: &Vec<f64>) {}
 
-    pub fn query(&self, inputs: &[i8]) -> Vec<i8> {
-        vec![]
+    pub fn query(&self, inputs: Vec<f64>) -> Vec<f64> {
+        let hidden_inputs = self.calculate_layer_output(inputs, &self.wih);
+        self.calculate_layer_output(hidden_inputs, &self.who)
+    }
+
+    fn calculate_layer_output(&self, inputs: Vec<f64>, weighting: &Vec<Vec<f64>>) -> Vec<f64> {
+        let inputs = math::transpose_vec(&vec![inputs]);
+        let weighted_input = match math::multiply_vectors(weighting, &inputs) {
+            Ok(r) => r,
+            Err(e) => panic!("{}", e),
+        };
+
+        let mut outputs: Vec<f64> = Vec::new();
+        for row in weighted_input {
+            // row.iter().sum() because there is only on value per row #shortcut
+            outputs.push((self.activation_function)(row.iter().sum()));
+        }
+        outputs
     }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
     fn create_new_neural_network() {
-        let nn = NeuralNetwork::new(3, 3, 3, 0.3, |x| x + 1);
+        let nn = NeuralNetwork::new(3, 3, 3, 0.3, |x| x + 1.0);
 
         assert_eq!(nn.input_nodes, 3);
         assert_eq!(nn.hidden_nodes, 3);
         assert_eq!(nn.output_nodes, 3);
         assert_eq!(nn.learning_rate, 0.3);
-        assert_eq!((nn.activation_function)(1), 2);
+        assert_eq!((nn.activation_function)(1.0), 2.0);
+    }
+
+    #[test]
+    fn test_train() {
+        let nn = NeuralNetwork::new(3, 3, 3, 0.3, |x| x + 1.0);
+
+        let inputs: Vec<f64> = vec![1.0];
+        let outputs: Vec<f64> = vec![1.0];
+
+        nn.train(inputs, &outputs);
     }
 
     #[test]
     fn test_query() {
-        let nn = NeuralNetwork::new(3, 3, 3, 0.3, |x| x + 1);
+        let nn = NeuralNetwork::new(3, 3, 3, 0.3, |x| x + 1.0);
 
-        let inputs: Vec<i8> = vec![1];
-        let outputs: Vec<i8> = vec![1];
+        let inputs: Vec<f64> = vec![1.0,1.0, 1.0];
 
-        nn.train(&inputs, &outputs);
+        nn.query(inputs);
     }
 }
--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,6 @@
 use rand;
 use rand::Rng;
+use std::f64::consts::E;
 
 pub fn create_weighting_vec(x: u64, y: u64) -> Vec<Vec<f64>> {
     let mut weighting_vec = Vec::new();
@@ -19,6 +20,9 @@ pub fn create_weighting_row(x: u64) -> Vec<f64> {
     row
 }
 
+pub fn sigmoid(x: f64) -> f64 {
+    ( 1.0 / (1.0 + E.powf(-x)))
+}
 
 
 #[cfg(test)]
@@ -28,15 +32,20 @@ mod tests {
     #[test]
     fn test_create_weighting_row() {
         let row = create_weighting_row(3);
-
         assert_eq!(row.len(), 3);
     }
 
     #[test]
     fn test_create_weighting_vec() {
         let vec = create_weighting_vec(3, 4);
-
         assert_eq!(vec.len(), 4);
         assert_eq!(vec[0].len(), 3);
+    }
+
+    #[test]
+    fn test_sigmoid() {
+        let y = 0.8807970779778823;
+        let x = sigmoid(2.0);
+        assert_eq!(y,x);
     }
 }
